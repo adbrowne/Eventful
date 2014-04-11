82f34a207fa410252fb61a50d731c79e0f0b5cb2
Andrew Browne <brownie@brownie.com.au>
2014-04-11 22:42:25 +1000

    Ensure consumer waits for value when queue is empty

--- src/Eventful.Tests/GroupingBoundedQueueTests.fs
+++ src/Eventful.Tests/GroupingBoundedQueueTests.fs
@@ -6,6 +6,7 @@ open System.Threading.Tasks
 open FsUnit
 
 module GroupingBoundedQueueTests = 
+
     [<Test>]
     let ``Can enqueue and dequeue an item`` () : unit = 
         let groupingQueue = new GroupingBoundedQueue<string,string>(1000, 100000)
@@ -25,5 +26,28 @@ module GroupingBoundedQueueTests =
                 items |> Seq.head |> should equal itemValue
                 items |> Seq.length |> should equal 1
             } |> Async.StartAsTask
+
+        consumer.Wait ()
+
+    [<Test>]
+    let ``Consumer will wait for value`` () : unit = 
+        let groupingQueue = new GroupingBoundedQueue<string,string>(1000, 100000)
+
+        let groupName = "group"
+        let itemValue = "item"
+
+        let waitMilliseconds = 200
+
+        let consumer =
+            async {
+                let stopwatch = System.Diagnostics.Stopwatch.StartNew()
+                do! groupingQueue.AsyncGet() |> Async.Ignore
+                stopwatch.ElapsedMilliseconds |> should be (greaterThanOrEqualTo waitMilliseconds)
+            } |> Async.StartAsTask
+
+        async {
+            do! Async.Sleep waitMilliseconds
+            do! groupingQueue.AsyncAdd(groupName, itemValue)
+        } |> Async.Start
 
         consumer.Wait ()
