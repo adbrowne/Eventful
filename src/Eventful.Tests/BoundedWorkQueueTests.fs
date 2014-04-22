3078cbb4571812fab1b2a6a6b0eec6bc8571100a
Andrew Browne <brownie@brownie.com.au>
2014-04-22 22:02:16 +1000

    Added performance test to BoundedWorkQueue

---
+++ src/Eventful.Tests/BoundedWorkQueueTests.fs
@@ -0,0 +1,18 @@
+﻿namespace Eventful.Tests
+
+open Eventful
+open System
+open Xunit
+open System.Threading.Tasks
+open FsUnit.Xunit
+#nowarn "40" // disable recursive definition warning
+module BoundedWorkQueueTests = 
+
+    [<Fact(Skip = "Slow performance test")>]
+    let ``Can enqueue and dequeue an item`` () : unit = 
+        let rec boundedWorkQueue = new BoundedWorkQueue<int>(1000, workQueued)
+
+        and workQueued item = async { boundedWorkQueue.WorkComplete(1) }
+
+        for i in [0..1000000] do
+            boundedWorkQueue.QueueWork i |> Async.RunSynchronously
