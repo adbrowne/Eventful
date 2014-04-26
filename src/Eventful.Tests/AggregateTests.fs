a93f520340bd7dafc68752c35aa8bedbddb739c4
Andrew Browne <brownie@brownie.com.au>
2014-04-26 16:51:42 +1000

    First experiments with aggregate and read store APIs

---
+++ src/Eventful.Tests/AggregateTests.fs
@@ -0,0 +1,100 @@
+﻿namespace Eventful.Tests
+
+open Eventful
+open Xunit
+open FsUnit.Xunit
+open FsCheck
+open FsCheck.Xunit
+open System
+
+type PersonCreatedEvt = {
+    Id : Guid
+    FirstName : string
+    LastName : string
+}
+
+type Eventful (aggregates : Aggregate<'TResult> seq, readModel : ReadModel) =
+    member this.RunCommand<'TCmd> (cmd:'TCmd) = 
+        let handler = 
+            aggregates 
+            |> Seq.toList
+            |> List.map (fun agg -> agg.GetCommandHandlers cmd)
+            |> List.collect (function 
+                            | Some handler -> [handler]
+                            | None -> [])
+            |> function
+            | [handler] -> handler
+            | [] -> failwith <| sprintf "No handlers found for command: %A" typeof<'TCmd>
+            | _ -> failwith <| sprintf "Multiple handlers found for command: %A" typeof<'TCmd>
+
+        let result = handler.Invoke (new Object()) cmd
+
+        readModel.RunEvent result
+
+type InMemoryDocumentStore () =
+    let mutable documents : Map<string, obj> = Map.empty
+    interface IDocumentStore with
+        member this.GetDocument<'TDocument> key =
+            match documents |> Map.containsKey key with
+            | true -> Some (documents.[key] :?> 'TDocument)
+            | false -> None
+        member this.Store<'TDocument> key (document : 'TDocument) =
+            documents <- documents |> Map.add key (document :> obj)
+            ()
+
+type CreatePersonCmd = {
+    Id : Guid
+    FirstName : string
+    LastName : string
+}
+
+[<CLIMutable>]
+type PersonDocument = {
+    Id : Guid
+    FirstName : string
+    LastName : string
+}
+
+module AggregateTests =
+    [<Fact>]
+    let ``Creating a person results in a person document`` () : unit =
+        let processPersonCmd _ (cmd : CreatePersonCmd) =
+            {
+                PersonCreatedEvt.Id = cmd.Id
+                FirstName = cmd.FirstName
+                LastName = cmd.LastName
+            }
+
+        let processPersonEvt (doc : PersonDocument) (evt : PersonCreatedEvt) =
+            { doc with
+                    Id = evt.Id
+                    FirstName = evt.FirstName
+                    LastName = evt.LastName }
+
+        let getPersonId (evt : PersonCreatedEvt) = "Person/" + evt.Id.ToString()
+
+        let personAggregate = Aggregate.Empty.AddCmdHandler(processPersonCmd)
+        let documentStore : IDocumentStore = new InMemoryDocumentStore() :> IDocumentStore
+
+        let emptyReadModel = (ReadModel.Empty documentStore)
+        let readModel = emptyReadModel.Add getPersonId processPersonEvt
+        let eventful = new Eventful(Seq.singleton personAggregate, readModel)
+
+        let id = Guid.NewGuid()
+
+        eventful.RunCommand {
+                CreatePersonCmd.Id = id
+                FirstName = "Andrew"            
+                LastName = "Browne"
+            }
+
+//        outEvent.Id |> should equal id
+//        outEvent.FirstName |> should equal "Andrew"
+//        outEvent.LastName |> should equal "Browne"
+
+        let (Some (andrewDoc : PersonDocument))  = documentStore.GetDocument("Person/" + id.ToString())
+        andrewDoc |> should not' (be Null)
+
+        andrewDoc.Id |> should equal id
+        andrewDoc.FirstName |> should equal "Andrew"
+        andrewDoc.LastName |> should equal "Browne"
---
+++ src/Eventful/Aggregate.fs
@@ -0,0 +1,40 @@
+﻿namespace Eventful
+    open System
+
+    type IFunctionInvoker<'TResult> =
+        abstract CmdType : Type
+        abstract StateType : Type
+        abstract Invoke : obj -> obj -> 'TResult
+
+    type IState =
+        abstract FoldState : obj -> obj -> obj
+        abstract Zero : obj
+        abstract StateType : Type
+
+    type internal FunctionWrapper<'TState, 'TCmd, 'TResult> (f : 'TState -> 'TCmd -> 'TResult) =
+        interface IFunctionInvoker<'TResult> with
+            member m.CmdType = typeof<'TCmd>
+            member m.StateType = typeof<'TState>
+            member m.Invoke (state : obj) (cmd : obj) =
+                f (state :?> 'TState) (cmd :?> 'TCmd)
+
+    type Aggregate<'TResult> (cmdHandlers : IFunctionInvoker<'TResult> list, stateGenerators : IState list) =
+        static let empty =
+            new Aggregate<'TResult>(List.empty, List.empty)
+
+        static member Empty : Aggregate<'TResult> = empty
+
+        member m.AddCmdHandler<'TState, 'TCmd> (f : 'TState -> 'TCmd -> 'TResult) =
+            let wrappedCmd = new FunctionWrapper<'TState, 'TCmd, 'TResult>(f) :> IFunctionInvoker<'TResult>
+            new Aggregate<'TResult>(wrappedCmd :: cmdHandlers, stateGenerators)
+
+        member m.AddStateGenerator<'TState> (stateGen : IState) =
+            new Aggregate<'TResult>(cmdHandlers, stateGen :: stateGenerators) 
+
+        member m.GetCommandHandlers<'TCmd> (cmd : 'TCmd) =
+            cmdHandlers
+            |> List.filter (fun x -> x.CmdType = typeof<'TCmd>) 
+            |> function
+            | [handler] -> Some handler
+            | [] -> None
+            | _ -> failwith <| sprintf "More than one handler for cmd: %A in aggregate %A" typeof<'TCmd> (m.GetType())
