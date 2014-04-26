b37782a4042c3c5b198214b2b6cb51ba0fb2bcf4
Andrew Browne <brownie@brownie.com.au>
2014-04-27 01:07:20 +1000

    Added tests to read streams while subscribing
    To ensure I didn't run into this issue: https://groups.google.com/forum/#!topic/event-store/mdxbAZdOCxA

--- src/Eventful.EventStore/EventStorePlay.fs
+++ src/Eventful.EventStore/EventStorePlay.fs
@@ -37,6 +37,46 @@
 
         } |> Async.RunSynchronously
 
+    [<Fact>]
+    let ``Read every stream as we run the events`` () : unit = 
+        let eventsMeter = Metrics.Meter(typeof<TestType>, "event", "events", TimeUnit.Seconds)
+        Metrics.EnableConsoleReporting(10L, TimeUnit.Seconds)
+        async {
+            printfn "Started"
+            let ipEndPoint = new System.Net.IPEndPoint(System.Net.IPAddress.Parse("127.0.0.1"), 1113)
+            let tcs = new System.Threading.Tasks.TaskCompletionSource<unit>()
+            let connectionSettingsBuilder = 
+                ConnectionSettings.Create()
+                                  .OnConnected(fun _ _ -> printf "Connected"; )
+                                  .OnErrorOccurred(fun _ ex -> printfn "Error: %A" ex)
+                                  .UseConsoleLogger()
+                                  .SetDefaultUserCredentials(new SystemData.UserCredentials("admin", "changeit"))
+
+            let connectionSettings : ConnectionSettings = ConnectionSettingsBuilder.op_Implicit(connectionSettingsBuilder)
+
+            let connection = EventStoreConnection.Create(connectionSettings, ipEndPoint)
+
+            connection.Connect()
+
+            let sw = System.Diagnostics.Stopwatch.StartNew()
+            let count = ref 0
+            let eventAppeared (event:ResolvedEvent) = 
+                System.Threading.Interlocked.Increment(count) |> ignore
+                async {
+                    do! connection.ReadStreamEventsForwardAsync(event.OriginalStreamId, 0, 1000, false) |> Async.AwaitTask |> Async.Ignore
+                } |> Async.Start
+                eventsMeter.Mark()
+
+            connection.SubscribeToAllFrom(System.Nullable(), false, (fun subscription event -> eventAppeared event), (fun subscription -> tcs.SetResult ())) |> ignore
+
+            do! tcs.Task |> Async.AwaitTask
+            printfn "All events read"
+
+            printfn "All events complete %d" !count
+
+        } |> Async.Start
+        
+        Async.Sleep (30000) |> Async.RunSynchronously
 
     [<Fact>]
     let ``Play eventstore events to null agent`` () : unit = 
