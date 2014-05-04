2cdd54da5effbf0e03d6841392204b3026268b9e
Andrew Browne <brownie@brownie.com.au>
2014-05-04 18:15:14 +1000

    Bump nuget version

---
+++ build/default.ps1
@@ -0,0 +1,39 @@
+properties {
+  $slnPath = "..\src\Eventful.sln"
+}
+
+task default -depends MsBuildRelease
+
+task Clean {
+  if (Test-Path .\package\lib) {
+  	remove-item .\package\lib -force -recurse
+  }
+  remove-item Eventful*.nupkg
+  exec { msbuild /t:Clean $slnPath }
+}
+
+task MsBuildDebug {
+  exec { msbuild /t:Build $slnPath -p:Configuration=Debug /maxcpucount:8 /verbosity:quiet}
+}
+
+task MsBuildRelease {
+  exec { msbuild /t:Build $slnPath -p:Configuration=Release /maxcpucount:8 /verbosity:quiet }
+}
+
+task Test -depends MsBuildRelease {
+	exec { & { ..\src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe .\Release\Eventful.Tests.dll }}
+}
+
+task Package -depends Clean, MsBuildRelease {
+  New-Item -force .\package\lib\net45 -itemtype directory
+  Copy-Item .\Release\Eventful.dll .\package\lib\net45
+  exec { & {.\tools\nuget\nuget.exe pack .\package\Eventful.nuspec -version 0.0.1.2 }}
+}
+
+task PackagePush -depends Package {
+  exec { & {.\tools\nuget\nuget.exe push Eventful.0.0.1.2.nupkg }}
+}
+
+task ? -Description "Helper to display task info" {
+	Write-Documentation
+}
