fcfbb12df2ab425a4ef11e274d06ed0dd0e748be
Andrew Browne <brownie@brownie.com.au>
2014-05-25 14:56:42 +1000

    Added a last complete item tracker

---
+++ src/Eventful.Tests/LastCompleteItemTracker.fs
@@ -0,0 +1,102 @@
+﻿namespace Eventful.Tests
+
+open Eventful
+open System
+open Xunit
+open System.Threading.Tasks
+open FsUnit.Xunit
+open FsCheck
+open FsCheck.Prop
+open FsCheck.Gen
+open FsCheck.Xunit
+open Eventful.Tests
+
+module LastCompleteItemTrackerTests = 
+    [<Fact>]
+    let ``Given empty When started Then lastComplete is None`` () : unit =  
+        let tracker = LastCompleteItemTracker.Empty
+        let result = tracker.Start 'a'
+        result.LastComplete |> should be Null // none shows up as Null in FsUnit
+
+    [<Fact>]
+    let ``Given started 'a' When competed 'a' Then lastComplete is 'a'`` () : unit =  
+        let tracker = LastCompleteItemTracker.Empty
+        let result = ((tracker.Start 'a').Complete 'a')
+        result.LastComplete |> should equal (Some 'a')
+
+    [<Fact>]
+    let ``Given [start 'a'; start 'b'; complete 'b'] When complete 'b' Then lastComplete is 'b'`` () : unit =  
+        let result = 
+            LastCompleteItemTracker.Empty
+            |> LastCompleteItemTracker.start 'a'
+            |> LastCompleteItemTracker.start 'b'
+            |> LastCompleteItemTracker.complete 'b'
+            |> LastCompleteItemTracker.complete 'a'
+
+        result.LastComplete |> should equal (Some 'b')
+
+    let rec insertAt list item index =
+        match (index, list) with
+        | (0, _) -> item::list
+        | (_, x::xs) -> x::(insertAt xs item (index - 1))
+        | (_, []) -> item::list
+
+    let allCompleteOperationsGen = 
+        let rec loop (acc : Item list) (value : int64) : Gen<Item list> = 
+            match value with
+            | 0L -> Gen.constant acc
+            | length -> gen { 
+                let startPosition = acc |> List.findIndex (fun x -> x = Start value)
+                let! endPosition = Gen.choose(startPosition + 1, acc.Length)
+                let finalAcc = insertAt acc (Item.Complete value) endPosition
+                return! (loop finalAcc (value - 1L)) }
+
+        Gen.sized (fun length -> loop ([1..length] |> List.map (fun x -> (int64 >> Start) x)) (int64 length))
+
+    let allCompleteOperations = { new Arbitrary<Item list>() with 
+        override x.Generator = allCompleteOperationsGen
+        override x.Shrinker t = Seq.empty }
+
+    let accumulator (acc : LastCompleteItemTracker<_>) op =
+        match op with
+        | Start value -> acc |> LastCompleteItemTracker.start value
+        | Complete value -> acc |> LastCompleteItemTracker.complete value 
+
+    [<Property>]
+    let ``When all items complete last complete is equal to the maximum item`` () =
+        forAll allCompleteOperations (fun (operations : Item list) -> 
+
+            let result = operations |> List.fold accumulator LastCompleteItemTracker<_>.Empty
+
+            if (operations.IsEmpty) then 
+                result.LastComplete |> should be Null // none shows up as Null in FsUnit
+            else
+                let expected = 
+                    operations
+                    |> List.map (function 
+                                | Start x -> x
+                                | Complete x -> x)
+                    |> List.max
+                    |> int64
+                    |> Some
+                result.LastComplete |> should equal expected
+        )
+
+
+    [<Property>]
+    let ``When last item is not complete then last complete is one less`` () =
+        forAll allCompleteOperations (fun (allOperations : Item list) -> 
+                if (allOperations.IsEmpty) then 
+                    ()
+                else
+                    let operations = allOperations |> Seq.take (allOperations.Length - 1) |> Seq.toList
+                    // last item must be a complete in a valid sequence
+                    let (Complete missingValue) = allOperations |> Seq.last
+
+                    let result = operations |> List.fold accumulator LastCompleteItemTracker<_>.Empty
+
+                    if missingValue = 1L then
+                        result.LastComplete |> should be Null // none shows up as Null in FsUnit
+                    else
+                        result.LastComplete |> should equal (Some (missingValue - 1L))
+        )
---
+++ src/Eventful/LastCompleteItemTracker.fs
@@ -0,0 +1,46 @@
+﻿namespace Eventful
+
+type LastCompleteItemTracker<'TItem when 'TItem : comparison> private 
+    (
+        items : Map<'TItem, int64>, 
+        completed: Set<int64>,
+        lastCompleteItem : 'TItem option,
+        lastStartedIndex : int64,
+        lastCompletedIndex: int64
+    ) =
+
+    let rec getMinimumCompleted (alreadyCompleted : Set<int64>) (currentMinimum : int64) =
+        let next = currentMinimum + 1L
+        if (alreadyCompleted |> Set.contains (currentMinimum + 1L)) then
+            getMinimumCompleted alreadyCompleted next
+        else
+            currentMinimum
+        
+    static let empty =
+        new LastCompleteItemTracker<'TItem>(Map.empty, Set.empty, None, -1L, -1L)
+
+    static member Empty = empty
+
+    member x.LastComplete = lastCompleteItem
+
+    member x.Start (item:'TItem) =
+        let index = lastStartedIndex + 1L
+        let items' = items |> Map.add item index
+        new LastCompleteItemTracker<'TItem>(items', completed, lastCompleteItem, index, lastCompletedIndex)
+        
+    member x.Complete (item:'TItem) =
+        let itemIndex = items.[item]
+        let completed' = completed |> Set.add itemIndex
+        let lastCompletedIndex' = getMinimumCompleted completed' lastCompletedIndex
+        let lastComplete' = items |> Map.tryFindKey (fun k v -> v = lastCompletedIndex')
+        let items' = items |> Map.filter (fun k v -> v > lastCompletedIndex')
+        let completed' = completed' |> Set.filter (fun v -> v > lastCompletedIndex')
+        new LastCompleteItemTracker<'TItem>(items', completed', lastComplete', lastStartedIndex, lastCompletedIndex')
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module LastCompleteItemTracker =
+    let start<'TItem when 'TItem : comparison> (item : 'TItem) (tracker : LastCompleteItemTracker<'TItem>) = 
+        tracker.Start item
+
+    let complete<'TItem when 'TItem : comparison> (item : 'TItem) (tracker : LastCompleteItemTracker<'TItem>) = 
+        tracker.Complete item
