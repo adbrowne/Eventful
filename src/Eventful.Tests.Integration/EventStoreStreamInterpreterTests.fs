83a97c7b4aaada3595d5321d23ca3c84dee95417
= <=>
2014-06-18 17:09:52 +1000

    Refactoring test runner

--- src/Eventful.Tests.Integration/EventStoreStreamInterpreterTests.fs
+++ src/Eventful.Tests.Integration/EventStoreStreamInterpreterTests.fs
@@ -28,34 +28,36 @@ module EventStoreStreamInterpreterTests =
 
             do! client.Connect()
 
+            let run program =
+                EventStreamInterpreter.interpret client RunningTests.esSerializer program
+
             let stream = "MyStream-" + (newId())
 
             let event = { Name = "Andrew Browne" }
             let metadata = { SourceMessageId = System.Guid.NewGuid(); MessageId = System.Guid.NewGuid() }
 
-            let writeARandomEvent = eventStream {
-                do! writeToStream stream EventStore.ClientAPI.ExpectedVersion.EmptyStream event metadata
-                return "Write Complete"
-             
-            }
+            let! writeResult = 
+                eventStream {
+                    do! writeToStream stream EventStore.ClientAPI.ExpectedVersion.EmptyStream event metadata
+                    return "Write Complete"
+                } |> run
 
-            let! writeResult = EventStreamInterpreter.interpret client RunningTests.esSerializer writeARandomEvent
             writeResult |> should equal "Write Complete"
 
-            let readEventProgram = eventStream {
-                let! item = readFromStream stream EventStore.ClientAPI.StreamPosition.Start
-                return!
-                    match item with
-                    | Some x -> 
-                        eventStream { 
-                            let! objValue = readValue x typeof<MyEvent>
-                            let value = objValue :?> MyEvent
-                            return Some value.Name
-                        }
-                    | None -> eventStream { return None } 
-            }
+            let! readResult =
+                eventStream {
+                    let! item = readFromStream stream EventStore.ClientAPI.StreamPosition.Start
+                    return!
+                        match item with
+                        | Some x -> 
+                            eventStream { 
+                                let! objValue = readValue x typeof<MyEvent>
+                                let value = objValue :?> MyEvent
+                                return Some value.Name
+                            }
+                        | None -> eventStream { return None } 
+                } |> run
 
-            let! readResult = EventStreamInterpreter.interpret client RunningTests.esSerializer readEventProgram
             readResult |> should equal (Some "Andrew Browne")
 
         } |> Async.RunSynchronously
