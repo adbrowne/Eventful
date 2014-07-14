1a54ecb027857166977e8213a0895b28071f2cf8
Andrew Browne <brownie@brownie.com.au>
2014-07-14 21:18:28 +1000

    Added C# wrapper for ParellelInOrderTransformer

---
+++ src/Eventful.CsTests/ParellelInOrderTransformerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Eventful.CsTests
+{
+    public class ParellelInOrderTransformerTest
+    {
+        [Fact]
+        public async Task CanRunItemsInParallel()
+        {
+            var rnd = new Random();
+
+            var tranformer = new CSharp.ParallelInOrderTransformer<int, int>(async v =>
+                {
+                    await Task.Delay(rnd.Next(10));
+                    return v;
+                }, 50, 5);
+
+            var list = new List<int>();
+
+            foreach(var i in Enumerable.Range(1, 100))
+            {
+                tranformer.Process(i, async result =>
+                    {
+                        await Task.Delay(rnd.Next(10));
+                        lock (list)
+                        {
+                            list.Add(result);
+                        }
+                    });
+            }
+
+            while (list.Count < 100)
+            {
+                await Task.Delay(100);
+            }
+
+            Assert.Equal(100, list.Count);
+            foreach (var item in list.Zip(Enumerable.Range(1, 100), (a, b) => a == b))
+            {
+                Assert.True(item);
+            }
+        }
+    }
+}
---
+++ src/Eventful/ParellelInOrderTransformer.CSharp.fs
@@ -0,0 +1,18 @@
+﻿namespace Eventful.CSharp
+
+open System
+open System.Collections.Generic
+open System.Runtime.InteropServices
+open System.Threading.Tasks
+
+type ParallelInOrderTransformer<'TInput,'TOutput>
+    (
+        work : System.Func<'TInput, Task<'TOutput>>, 
+        maxItems : int, 
+        workers : int
+    ) =
+
+    let transformer = new Eventful.ParallelInOrderTransformer<'TInput, 'TOutput>((fun i -> work.Invoke(i) |> Async.AwaitTask), maxItems, workers)
+
+    member x.Process(input: 'TInput, onComplete : System.Func<'TOutput, Task>) : unit =
+        transformer.Process(input, (fun o -> onComplete.Invoke(o) |> Async.AwaitIAsyncResult |> Async.Ignore)) 
