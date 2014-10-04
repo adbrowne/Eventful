4b1444fe43bf0450d284bb036be10ecfb98430e7
Andrew Browne <brownie@brownie.com.au>
2014-10-04 20:56:48 +1000

    Added new UnitStateBuilder

---
+++ src/Eventful.Tests/AggregateStateBuilderTests.fs
@@ -0,0 +1,74 @@
+﻿namespace Eventful.Tests
+
+open System
+
+open Xunit
+open FsUnit.Xunit
+
+open Eventful
+
+module AggregateStateBuilderTests = 
+    type SampleMetadata = {
+        Tenancy : string
+        AggregateId : Guid 
+    }
+    with static member Emtpy = { Tenancy = ""; AggregateId = Guid.Empty }
+
+    type WidgetCreatedEvent = {
+        WidgetId : Guid
+        Name : string
+    }
+
+    type WidgetRenamedEvent = {
+        WidgetId : Guid
+        NewName : string
+    }
+
+    type WidgetNameAppendedToEvent = {
+        WidgetId : Guid
+        Suffix : string
+    }
+
+    let widgetNameStateBuilder : UnitStateBuilder<string,SampleMetadata,Guid> = 
+        UnitStateBuilder.Empty "WidgetName" ""
+        |> UnitStateBuilder.handler (fun (e:WidgetCreatedEvent) m -> e.WidgetId) (fun (s,e,m) -> e.Name)
+        |> UnitStateBuilder.handler (fun (e:WidgetRenamedEvent) m -> e.WidgetId) (fun (s,e,m) -> e.NewName)
+        |> UnitStateBuilder.handler (fun (e:WidgetNameAppendedToEvent) m -> e.WidgetId) (fun (s,e,m) -> sprintf "%s%s" s e.Suffix)
+
+    let widgetEventCountBuilder : UnitStateBuilder<int, SampleMetadata, Guid> =
+        UnitStateBuilder.Empty "EventCount" 0
+        |> UnitStateBuilder.allEventsHandler (fun m -> m.AggregateId) (fun (s,e,m) -> s + 1)
+
+    let metadata = { Tenancy = "Blue"; AggregateId = Guid.NewGuid() }
+    let widgetId = Guid.Parse("2F8A016D-FB2C-4857-8E2F-5E81FB4F95DA")
+
+    [<Fact>]
+    let ``Can calculate simple state`` () =
+        (widgetNameStateBuilder, widgetNameStateBuilder.InitialState)
+        |> UnitStateBuilder.run { WidgetId = widgetId; Name = "My Widget Name"} metadata
+        |> snd
+        |> should equal "My Widget Name"
+
+    [<Fact>]
+    let ``Can run multiple events`` () =
+        (widgetNameStateBuilder, widgetNameStateBuilder.InitialState)
+        |> UnitStateBuilder.run { WidgetId = widgetId; Name = "My Widget Name"} metadata
+        |> UnitStateBuilder.run { WidgetId = widgetId; NewName = "My NEW Widget Name"} metadata
+        |> snd
+        |> should equal "My NEW Widget Name"
+
+    [<Fact>]
+    let ``Can use previous state`` () =
+        (widgetNameStateBuilder, widgetNameStateBuilder.InitialState)
+        |> UnitStateBuilder.run { WidgetId = widgetId; Name = "My Widget Name"} metadata
+        |> UnitStateBuilder.run { WidgetId = widgetId; Suffix = "My Suffix"} metadata
+        |> snd
+        |> should equal "My Widget NameMy Suffix"
+
+    [<Fact>]
+    let ``Can count events`` () =
+        (widgetEventCountBuilder, widgetEventCountBuilder.InitialState)
+        |> UnitStateBuilder.run { WidgetId = widgetId; Name = "My Widget Name"} metadata
+        |> UnitStateBuilder.run { WidgetId = widgetId; Name = "My Widget Name"} metadata
+        |> snd
+        |> should equal 2
---
+++ src/Eventful/AggregateStateBuilder.fs
@@ -0,0 +1,61 @@
+﻿namespace Eventful
+
+open System
+
+type IAggregateStateBuilder<'TState, 'TMetadata, 'TKey> = 
+    abstract Name : string
+    abstract Apply : 'TState * obj * 'TMetadata -> 'TState
+    abstract InitialState : 'TState
+    abstract MessageTypes : List<Type>
+    abstract GetKey : obj * 'TMetadata -> 'TKey
+
+type HandlerFunction<'TState, 'TMetadata, 'TEvent> = 'TState * 'TEvent * 'TMetadata -> 'TState
+type GetAllEventsKey<'TMetadata, 'TKey> = 'TMetadata -> 'TKey
+type GetEventKey<'TMetadata, 'TEvent, 'TKey> = 'TEvent -> 'TMetadata -> 'TKey
+
+type UnitStateBuilderHandler<'TState, 'TMetadata, 'TKey> = 
+    | AllEvents of GetAllEventsKey<'TMetadata, 'TKey> * HandlerFunction<'TState, 'TMetadata, obj>
+    | SingleEvent of Type * GetEventKey<'TMetadata, obj, 'TKey> * HandlerFunction<'TState, 'TMetadata, obj>
+
+type UnitStateBuilder<'TState, 'TMetadata, 'TKey>(name: string, initialState : 'TState, handlers : UnitStateBuilderHandler<'TState, 'TMetadata, 'TKey> list) = 
+    static member Empty name initialState = new UnitStateBuilder<'TState, 'TMetadata, 'TKey>(name, initialState, List.empty)
+    member x.InitialState = initialState
+    member x.AddHandler<'T> (f:UnitStateBuilderHandler<'TState, 'TMetadata, 'TKey>) =
+        new UnitStateBuilder<'TState, 'TMetadata, 'TKey>(name, initialState, f::handlers)
+        
+    member x.Run (currentState: 'TState) (item: obj) (metadata: 'TMetadata) =
+        let acc state handler =
+            match handler with
+            | AllEvents (getKey, handlerFunction) ->
+                handlerFunction (state, item, metadata)
+            | SingleEvent (eventType, getKey, handlerFunction) ->
+                if item.GetType() = eventType then
+                    handlerFunction (state, item, metadata)
+                else
+                    state
+        handlers
+        |> List.fold acc currentState
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module UnitStateBuilder =
+    let untypedHandler f (state, (evt : obj), metadata) = 
+        match evt with
+        | :? 'TEvent as evt ->
+            f (state, evt, metadata) 
+        | _ -> failwith <| sprintf "Expecting type: %s but got type: %s" typeof<'TEvent>.FullName (evt.GetType().FullName)
+
+    let untypedGetKey f (evt : obj) metadata = 
+        match evt with
+        | :? 'TEvent as evt ->
+            f evt metadata
+        | _ -> failwith <| sprintf "Expecting type: %s but got type: %s" typeof<'TEvent>.FullName (evt.GetType().FullName)
+
+    let handler (getKey : GetEventKey<'TMetadata, 'TEvent, 'TKey>) (f : HandlerFunction<'TState, 'TMetadata, 'TEvent>) (b : UnitStateBuilder<'TState, 'TMetadata, 'TKey>) =
+        b.AddHandler <| SingleEvent (typeof<'TEvent>, untypedGetKey getKey, untypedHandler f)
+
+    let allEventsHandler getKey f (b : UnitStateBuilder<'TState, 'TMetadata, 'TKey>) =
+        b.AddHandler <| AllEvents (getKey, untypedHandler f)
+
+    let run (evt : 'TEvent) (metadata : 'TMetadata) (builder: UnitStateBuilder<'TState, 'TMetadata, 'TKey> , currentState : 'TState) =
+        let state' = builder.Run currentState (evt :> obj) metadata
+        (builder, state')
