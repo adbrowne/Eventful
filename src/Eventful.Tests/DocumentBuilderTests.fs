3f4714de02f8fa069597c9b140678b5e501ad732
Andrew Browne <brownie@brownie.com.au>
2014-10-07 22:43:39 +1100

    Use unit state build for document

--- src/Eventful.Tests/DocumentBuilderTests.fs
+++ src/Eventful.Tests/DocumentBuilderTests.fs
@@ -24,11 +24,13 @@ module DocumentBuilderTests =
         NewName : string
     }
 
-    let widgetNameStateBuilder : KeyedStateBuilder<string,SampleMetadata,Guid> = 
-        StateBuilder.Empty ""
-        |> StateBuilder.addHandler (fun s (e:WidgetCreatedEvent) -> e.Name)
-        |> StateBuilder.addHandler (fun s (e:WidgetRenamedEvent) -> e.NewName)
-        |> NamedStateBuilder.withKey "WidgetName" (fun m -> m.AggregateId)
+    let inline getWidgetId (a: ^a) _ = 
+        (^a : (member WidgetId: Guid) (a))
+
+    let widgetNameStateBuilder : UnitStateBuilder<string,SampleMetadata,Guid> = 
+        UnitStateBuilder.Empty "WidgetName" ""
+        |> UnitStateBuilder.handler getWidgetId (fun (s, (e:WidgetCreatedEvent), m) -> e.Name)
+        |> UnitStateBuilder.handler getWidgetId (fun (s, (e:WidgetRenamedEvent), m) -> e.NewName)
 
     type WidgetDocument = {
         WidgetId : Guid
@@ -36,46 +38,54 @@ module DocumentBuilderTests =
     with static member Empty id = { WidgetId = id; Name = "" }
 
     type IDocumentStateMap<'TDocument, 'TMetadata, 'TKey> =
-        abstract member Types : List<Type>
-        abstract member Apply : 'TDocument * obj * 'TMetadata -> 'TDocument
-        abstract member GetKey : (obj * 'TMetadata) -> 'TKey
+        abstract member GetKey<'TEvent> : 'TEvent * 'TMetadata -> 'TKey seq
+        abstract member Apply<'TEvent> : 'TKey * 'TDocument * 'TEvent * 'TMetadata -> 'TDocument
 
     type DocumentBuilder<'TKey,'T, 'TMetadata when 'TKey : equality>(createDoc:'TKey -> 'T, getDocumentKey:'TKey -> string, stateMaps: IDocumentStateMap<'T, 'TMetadata, 'TKey> list) =
         static member Empty<'TKey,'T> createDoc getDocumentKey = new DocumentBuilder<'TKey,'T, 'TMetadata>(createDoc, getDocumentKey, [])
         member x.AddStateMap stateMap =
             new DocumentBuilder<'TKey,'T, 'TMetadata>(createDoc, getDocumentKey, stateMap::stateMaps)
-        member x.EventTypes =
-            stateMaps |> List.collect (fun x -> x.Types)
         member x.GetDocumentKey = getDocumentKey
-        member x.GetKeysFromEvent (evt:obj, metadata : 'TMetadata) : 'TKey list =
+        member x.GetKeysFromEvent (evt:'TEvent, metadata : 'TMetadata) : 'TKey list =
             stateMaps 
             |> List.map (fun x -> x.GetKey (evt, metadata))
-            |> Seq.ofList
+            |> Seq.collect id
             |> Seq.distinct
             |> List.ofSeq
 
     [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
     module DocumentBuilder =
-        let mapStateToProperty (sb:IKeyedStateBuilder<'TProperty, 'TMetadata,'TKey>) (getter:'T -> 'TProperty) (setter:'TProperty -> 'T -> 'T) (builder : DocumentBuilder<'TKey,'T, 'TMetadata>)  =
+        let mapStateToProperty (sb:UnitStateBuilder<'TProperty, 'TMetadata,'TKey>) (getter:'T -> 'TProperty) (setter:'TProperty -> 'T -> 'T) (builder : DocumentBuilder<'TKey,'T, 'TMetadata>)  =
            let stateMap = 
                {
                    new IDocumentStateMap<'T, 'TMetadata, 'TKey> with 
-                       member this.Types = sb.MessageTypes
-                       member this.Apply (document, msg, metadata) = 
+                       member this.Apply (key, document, evt, metadata) = 
                             let currentValue = getter document
-                            let updated = sb.Apply(currentValue, msg, metadata)
+                            let handlers = 
+                                sb.GetRunners<'TEvent>()
+                                |> Seq.map (fun (getKey,runner) -> (getKey evt metadata, runner))
+                                |> Seq.filter(fun (k, _) -> k = key)
+                                |> Seq.map snd
+
+                            let runHandler v h = h evt metadata v
+
+                            let updated = handlers |> Seq.fold runHandler currentValue
                             setter updated document
-                       member this.GetKey f = sb.GetKey f
+                       member this.GetKey<'TEvent>(evt : 'TEvent, metadata) = 
+                            sb.GetRunners<'TEvent>()
+                            |> Seq.map fst
+                            |> Seq.map (fun f -> f evt metadata)
                }
            builder.AddStateMap(stateMap)
 
     [<Fact>]
+    [<Trait("category", "unit")>]
     let ``Can map state builders to documents`` () =
         let visitDocumentBuilder = 
             DocumentBuilder.Empty<Guid, WidgetDocument> (fun x -> WidgetDocument.Empty x) (fun x -> sprintf "WidgetDocument/%s" (x.ToString()))
             |> DocumentBuilder.mapStateToProperty widgetNameStateBuilder (fun doc -> doc.Name) (fun name doc -> { doc with Name = name })
 
-        visitDocumentBuilder.EventTypes |> List.exists (fun x -> x = typeof<WidgetRenamedEvent>) |> should equal true
+        // visitDocumentBuilder.EventTypes |> List.exists (fun x -> x = typeof<WidgetRenamedEvent>) |> should equal true
         let guid = Guid.Parse("75ca0d81-7a8e-4692-86ac-7f128deb75bd")
         visitDocumentBuilder.GetDocumentKey guid |> should equal "WidgetDocument/75ca0d81-7a8e-4692-86ac-7f128deb75bd"
 
