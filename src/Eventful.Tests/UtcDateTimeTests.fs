b89747ce0a390aabb2397792cb7c7139c1825004
Andrew Browne <brownie@brownie.com.au>
2014-12-24 15:52:58 +1100

    Added UtcDateTime class

---
+++ src/Eventful.Tests/UtcDateTimeTests.fs
@@ -0,0 +1,82 @@
+﻿namespace Eventful.Tests
+
+open System
+open Xunit
+open FsCheck.Xunit
+open FsCheck
+open Eventful
+open Swensen.Unquote
+
+module UtcDateTimeTests = 
+
+    type Generators = 
+        static member DateTime() =
+            gen {
+                let! (DontSize (value : int64)) = Arb.generate
+                let value = Math.Abs(value) % DateTime.MaxValue.Ticks //make it in range
+                return new DateTime(value, DateTimeKind.Utc)
+            }
+            |> Arb.fromGen
+
+        static member UtcDateTime() =
+            gen {
+                let! (DontSize (value : int64)) = Arb.generate
+                let value = Math.Abs(value) % DateTime.MaxValue.Ticks //make it in range
+                return { UtcDateTime.Ticks = value }
+            }
+            |> Arb.fromGen
+
+    let roundTripViaString =
+        UtcDateTime.fromDateTime
+        >> UtcDateTime.toString
+        >> UtcDateTime.fromString
+        >> UtcDateTime.toDateTime
+        
+    [<Property(Arbitrary=[| typeof<Generators> |])>]
+    [<Trait("category", "unit")>]
+    let ``DateTime can round trip to string with zero padding`` (dateTime : DateTime) = 
+        dateTime
+        |> roundTripViaString
+        |> ((=) dateTime)
+
+    [<Property(Arbitrary=[| typeof<Generators> |])>]
+    [<Trait("category", "unit")>]
+    let ``String version of DateTime obeys DateTime ordering`` (dateTime1 : DateTime) (dateTime2 : DateTime) = 
+        let stringDateTime1 = dateTime1 |> UtcDateTime.fromDateTime |> UtcDateTime.toString
+        let stringDateTime2 = dateTime2 |> UtcDateTime.fromDateTime |> UtcDateTime.toString
+        (stringDateTime1 < stringDateTime2) = (dateTime1 < dateTime2)
+
+    [<Property(Arbitrary=[| typeof<Generators> |])>]
+    [<Trait("category", "unit")>]
+    let ``UtcDateTime obeys DateTime ordering`` (dateTime1 : DateTime) (dateTime2 : DateTime) = 
+        let utcDateTime1 = dateTime1 |> UtcDateTime.fromDateTime
+        let utcDateTime2 = dateTime2 |> UtcDateTime.fromDateTime
+        (utcDateTime1 < utcDateTime2) = (dateTime1 < dateTime2)
+
+    let unspecifiedDateTime = new DateTime(2007, 1, 1, 0, 0, 0, DateTimeKind.Unspecified)
+
+    [<Fact>]
+    [<Trait("category", "unit")>]
+    let ``Unspecified datetime kind raises exception`` () =
+        raisesWith 
+            <@ UtcDateTime.fromDateTime unspecifiedDateTime @> 
+            (fun (e : System.Exception) -> <@ e.Message = "Unknown DateTimeKind: Unspecified"@>)
+
+    [<Fact>]
+    [<Trait("category", "unit")>]
+    let ``Local time is round tripped`` () =
+        let localDateTime = DateTime.SpecifyKind(unspecifiedDateTime, DateTimeKind.Local)
+
+        localDateTime
+        |> roundTripViaString
+        |> ((=) localDateTime)
+
+    [<Fact>]
+    [<Trait("category", "unit")>]
+    let ``Time coming back from UtcDateTime is Utc`` () =
+        let localDateTime = DateTime.SpecifyKind(unspecifiedDateTime, DateTimeKind.Local)
+
+        localDateTime
+        |> roundTripViaString
+        |> (fun x -> x.Kind)
+        =? DateTimeKind.Utc
---
+++ src/Eventful/UtcDateTime.fs
@@ -0,0 +1,33 @@
+﻿namespace Eventful
+
+open System
+
+// DateTime type that is unambiguous
+type UtcDateTime = {
+    // The value of this property represents the number of 
+    // 100-nanosecond intervals that have elapsed since 12:00:00 midnight, January 1, 0001
+    Ticks : int64
+}
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module UtcDateTime = 
+    /// throws if DateTimeKind is not Local or Utc
+    /// DateTime type should aways be specified
+    let fromDateTime (dateTime : DateTime) =
+        match dateTime.Kind with
+        | DateTimeKind.Local ->
+            { UtcDateTime.Ticks = dateTime.ToUniversalTime().Ticks }
+        | DateTimeKind.Utc ->
+            { UtcDateTime.Ticks = dateTime.Ticks }
+        | _ -> failwith <| sprintf "Unknown DateTimeKind: %A" dateTime.Kind
+
+    let toDateTime (utcDateTime : UtcDateTime) =
+        new DateTime(utcDateTime.Ticks, DateTimeKind.Utc)
+
+    let toString (utcDateTime : UtcDateTime) =
+        utcDateTime.Ticks.ToString("D21")
+
+    let fromString (str : string) = 
+        str 
+        |> System.Int64.Parse
+        |> (fun x -> { UtcDateTime.Ticks = x })
