415f74f702173ac0254c0d75f21ed00f6c5a340b
Andrew Browne <brownie@brownie.com.au>
2015-01-06 20:11:03 +1100

    Add setup database step to BookLibrary runner

--- src/Eventful.Tests.Integration/BookLibraryRunner.fs
+++ src/Eventful.Tests.Integration/BookLibraryRunner.fs
@@ -17,14 +17,33 @@ module BookLibraryRunner =
 
     let private executableAbsolutePath = Path.Combine(IntegrationTests.bookLibraryExecutableDirectory, "BookLibrary.exe")
 
+    let commonArgs (applicationConfig : BookLibrary.ApplicationConfig) = seq {
+            yield CLIArguments.EventStore (applicationConfig.EventStore.Server, applicationConfig.EventStore.TcpPort)
+            yield CLIArguments.RavenServer (applicationConfig.Raven.Server, applicationConfig.Raven.Port)
+            yield CLIArguments.RavenDatabase (applicationConfig.Raven.Database)
+        }
+        
+    let setupDatabase (applicationConfig : BookLibrary.ApplicationConfig) =
+        let processArguments = 
+            seq {
+                yield! commonArgs applicationConfig
+                yield CLIArguments.Create_Raven_Database
+            }
+            |> List.ofSeq
+            |> CLIArguments.Parser.PrintCommandLineFlat 
+
+        let bookLibraryProcess = IntegrationTests.startProcess executableAbsolutePath processArguments
+        bookLibraryProcess.WaitForExit()
+
+    let dataReceived blah =
+        ()
+
     let startNewProcess (applicationConfig : BookLibrary.ApplicationConfig) =
         let httpPort = IntegrationTests.findFreeTcpPort()
 
         let processArguments = 
             seq {
-                yield CLIArguments.EventStore (applicationConfig.EventStore.Server, applicationConfig.EventStore.TcpPort)
-                yield CLIArguments.RavenServer (applicationConfig.Raven.Server, applicationConfig.Raven.Port)
-                yield CLIArguments.RavenDatabase (applicationConfig.Raven.Database)
+                yield! commonArgs applicationConfig
                 yield CLIArguments.WebServer ("localhost", httpPort)
             }
             |> List.ofSeq
