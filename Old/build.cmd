37fbcc9a7c3a8a53d507256006011c72d6db6de9
Andrew Browne <brownie@brownie.com.au>
2015-01-24 18:28:19 +1100

    Moving/Removing old files

--- Old/build.cmd
+++
@@ -1,2 +0,0 @@
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\build\build.ps1' %*;"
-exit /B %errorlevel%
--- Old/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mono --runtime=v4.0 src/.nuget/NuGet.exe restore src/Eventful.sln
-xbuild src/Eventful.sln
